b9f333922e837570f1a14f1703d114fd6540bcb0
luckymancvp <luckymancvp@gmail.com>
2013-06-11 19:06:45 +0900

    g

--- protected/runtime/gii-1.1.13/ModelCode.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'template' => 'LogicDAO',
-  'connectionId' => 'db',
-  'tablePrefix' => '',
-  'modelPath' => 'application.models',
-  'baseClass' => 'CActiveRecord',
-  'buildRelations' => '1',
-);
