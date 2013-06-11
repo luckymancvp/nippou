a6e5f6528f107c04ae8d612411394c8428b15936
luckymancvp <luckymancvp@gmail.com>
2013-06-11 14:58:12 +0900

    add import command

---
+++ protected/commands/ImportDBCommand.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: luckymancvp
+ * Date: 2/15/13
+ * Time: 8:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ImportDBCommand extends CConsoleCommand {
+    public function run($args)
+    {
+    	$db = $this->getDBInfo();
+    	$link = $this->connect($db);
+
+    	//$sql  = "DROP DATABASE IF EXISTS {$db["dbName"]} ;";
+        //$sql .= "CREATE DATABASE IF NOT EXISTS {$db["dbName"]} ;";
+        $sql = "";
+
+        $dataPath = Yii::getPathOfAlias("application"). "/data";
+        if ($handle = opendir($dataPath)){
+        	while (false !== ($entry = readdir($handle))){
+        		if ($this->getExt($entry) == "sql"){
+        			$sql .= file_get_contents($dataPath . "/". $entry);
+        		}
+        	}
+        	closedir($handle);
+        }
+
+        echo $sql;
+        mysql_query($sql);
+    }
+
+    private function getDBInfo(){
+    	$config = Yii::app()->getComponents(false);
+    	$connString = $config[db][connectionString];
+        $connArr    = preg_split("/[=;]/", $connString);
+
+        return array(
+        	'host'     => $connArr[1],
+        	'dbName'   => $connArr[3],
+        	'username' => $config[db]["username"],
+        	'password' => $config[db]["password"],
+        );
+    }
+
+    private function connect($db){
+    	$link = mysql_connect($db["host"], $db["username"], $db["password"]);
+		if (!$link) {
+		    die('Could not connect: ' . mysql_error());
+		}
+		echo 'Connected successfully';
+		return $link;
+    }
+
+    /**
+     * Get extension of a file 
+     *
+    */ 
+    private function getExt($entry){
+    	$arr = explode(".", $entry);
+    	return $arr[count($arr) - 1];
+    }
+}
